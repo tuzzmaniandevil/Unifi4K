8da3335b7f41020612821b78e0cb8927a701f2e4
tuzzm <tuzzm@DESKTOP-2O1E47R>
2016-04-18 21:06:16 +1200

    Added more methods

--- APP-INF/unifi-0.0.1.js
+++ APP-INF/unifi-0.0.1.js
@@ -3,14 +3,13 @@
         var _self = this;
 
         _self._cookie = '';
-        _self._loginAttempt = 0;
 
         _self._options = $.extend({
             hostname: 'localhost',
             port: 8443,
             site: 'default',
-            username: 'admin',
-            password: 'admin',
+            username: 'ubnt',
+            password: 'ubnt',
             autoLogin: true
         }, options);
     };
@@ -44,7 +43,6 @@
         };
 
         var data = JSON.stringify(params);
-        log.info('Data: {}', data);
 
         xmlhttp.send(data);
 
@@ -77,27 +75,9 @@
     };
 
     /**
-     * Get a list of vouchers for this site
-     * @param {type} cb - Called when GET is finished, params (err {boolean}, data {json})
-     * @returns {JSON}
-     */
-    UniFi.prototype.getVouchers = function (cb) {
-        var _self = this;
-
-        return _self._doGet('/api/s/' + _self._options.site + '/stat/voucher', cb);
-    };
-
-    UniFi.prototype.deleteVoucher = function (id, cb) {
-        var params = {
-            'cmd': 'delete-voucher',
-            '_id': id
-        };
-    };
-
-    /**
      * Get a list of devices for this site
-     * @param {type} cb
      * @param {type} site - the site name to use, if none specified the options default will be used
+     * @param {type} cb
      * @returns {Array|Object|text}
      */
     UniFi.prototype.getDevices = function (site, cb) {
@@ -125,50 +105,125 @@
     };
 
     /**
+     * Get a list of vouchers for this site
+     * @param {type} cb - Called when GET is finished, params (err {boolean}, data {json})
+     * @returns {JSON}
+     */
+    UniFi.prototype.getVouchers = function (cb) {
+        var _self = this;
+
+        return _self._doGet('/api/s/' + _self._options.site + '/stat/voucher', cb);
+    };
+
+    /**
+     * Deletes a voucher with the specified ID
+     * @param {type} id - The ID of the voucher to delete
+     * @param {type} site - the site name to use, if none specified the options default will be used
+     * @param {type} cb
+     * @returns {JSON}
+     */
+    UniFi.prototype.deleteVoucher = function (id, site, cb) {
+        var _self = this;
+
+        if (typeof site === 'function') {
+            cb = site;
+            site = null;
+        }
+
+        var s = site || _self._options.site;
+
+        var params = {
+            'cmd': 'delete-voucher',
+            '_id': id
+        };
+
+        return _self._doPost('/api/s/' + s + '/cmd/hotspot', params, cb);
+    };
+
+    /**
      *
      * @param {String} path
      * @param {Function} cb - Called when GET is finished, params (err {boolean}, data {json})
+     * @param {boolean} autologin - (optional) Overrides the auto login flag
      */
     UniFi.prototype._doGet = function (path, cb, autologin) {
+        var _self = this;
+
+        return _self._doHttpRequest(path, 'GET', null, cb, autologin);
+    };
+
+    /**
+     * 
+     * @param {type} path
+     * @param {type} data - a String or JSON (which will be stringified) to send as the body
+     * @param {type} cb - Called when POST is finished, params (err {boolean}, data {json})
+     * @param {type} autologin - (optional) Overrides the auto login flag
+     * @returns {JSON}
+     */
+    UniFi.prototype._doPost = function (path, data, cb, autologin) {
+        var _self = this;
+
+        return _self._doHttpRequest(path, 'POST', data, cb, autologin);
+    };
+
+    /**
+     * 
+     * @param {type} path
+     * @param {type} type - The type of request to make, e.g. POST or GET
+     * @param {type} data - a String or JSON (which will be stringified) to send as the body
+     * @param {type} cb - Called when the request is finished, params (err {boolean}, data {json})
+     * @param {type} autologin - (optional) Overrides the auto login flag
+     * @returns {JSON}
+     */
+    UniFi.prototype._doHttpRequest = function (path, type, data, cb, autologin) {
         var _self = this;
 
         var al = autologin || _self._options.autoLogin;
 
         var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", 'https://' + _self._options.hostname + ':' + _self._options.port + path, false);
+        xmlhttp.open(type, 'https://' + _self._options.hostname + ':' + _self._options.port + path, false);
         xmlhttp.setRequestHeader('Cookie', _self._cookie);
 
-        var data = null;
+        var result = null;
 
         xmlhttp.onloadend = function () {
             var resp = xmlhttp.response;
             var statusCode = resp.statusCode;
             var text = xmlhttp.responseText;
-            if (text != null && text.length > 0) {
-                data = JSON.parse(xmlhttp.responseText);
-            } else {
-                data = text;
+            if (text.length > 0) {
+                try {
+                    result = JSON.parse(xmlhttp.responseText);
+                } catch (err) {
+                    // TODO Handle error parsing JSON
+                }
             }
 
             if (statusCode === 401 && al) { // Not logged in and auto login enabled.
                 var result = _self.login();
                 if (result) {
-                    data = _self._doGet(path, cb);
+                    result = _self._doGet(path, cb);
                 } else {
-                    cb(true, data);
+                    cb(true, result);
                 }
             } else if (cb && typeof cb === 'function') {
                 var err = false;
                 if (statusCode !== 200) {
                     err = true;
                 }
-                cb(err, data);
+                cb(err, result);
             }
         };
 
-        xmlhttp.send();
+        if (data !== null && typeof data !== 'undefined') {
+            if (data.constructor === Object) {
+                data = JSON.stringify(data);
+            }
+            xmlhttp.send(data);
+        } else {
+            xmlhttp.send();
+        }
 
-        return data;
+        return result;
     };
 
     UniFi.version = UniFi.prototype.version = '0.0.1';
